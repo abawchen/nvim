bd186aaac6490dc8c9af769efe7ff6bba4930689
abaw.chen <abaw.chen@gmail.com>
2024-04-06 09:56:57 +0800

    feat: baseline

--- lua/plugin-config/mason.lua
+++ lua/plugin-config/mason.lua
@@ -93,26 +93,6 @@ null_ls.setup({
 	-- end,
 })
 
--- -- local lspconfig = require("lspconfig")
--- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
---
--- lspconfig.gopls.setup({
--- 	capabilities = lsp_capabilities,
--- 	-- https://stackoverflow.com/a/73289654
--- 	-- on_attach = require("cmp").on_attach,
--- 	settings = {
--- 		gopls = {
--- 			buildFlags = { "-tags=ems bsc cht rakuten taipower wireinject" },
--- 		},
--- 	},
--- })
--- lspconfig.pyright.setup({
--- 	capabilities = lsp_capabilities,
--- })
--- lspconfig.jdtls.setup({
--- 	capabilities = lsp_capabilities,
--- })
-
 -- XXX: it works!
 -- https://neovim.discourse.group/t/lsp-formatting-doesnt-always-work/1884/20
 vim.api.nvim_create_augroup("LspFormatting", { clear = true })
--- lua/plugin-config/nvim-lspconfig.lua
+++ lua/plugin-config/nvim-lspconfig.lua
@@ -26,23 +26,3 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
 	vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, diagnostic_config)
 vim.api.nvim_set_keymap("n", "dp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", "dn", "<cmd>lua vim.diagnostic.goto_next()<CR>", { silent = true, noremap = true })
-
--- local lspconfig = require("lspconfig")
--- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
---
--- lspconfig.gopls.setup({
--- 	capabilities = lsp_capabilities,
--- 	-- https://stackoverflow.com/a/73289654
--- 	-- on_attach = require("cmp").on_attach,
--- 	settings = {
--- 		gopls = {
--- 			buildFlags = { "-tags=ems bsc cht rakuten taipower wireinject" },
--- 		},
--- 	},
--- })
--- lspconfig.pyright.setup({
--- 	capabilities = lsp_capabilities,
--- })
--- lspconfig.jdtls.setup({
--- 	capabilities = lsp_capabilities,
--- })

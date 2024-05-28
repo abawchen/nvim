3b0c7fab7753f8400f60bf19095038f3de7b7562
abaw.chen <abaw.chen@gmail.com>
2024-05-28 10:23:35 +0800

    feat: make ctrl-/ work

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- https://vi.stackexchange.com/a/39338/27441
+keymap.set("n", "<c-/>", "gcc", { remap = true })
+keymap.set("n", "<c-_>", "gcc", { remap = true })
+keymap.set("v", "<c-/>", "gc", { remap = true })
+keymap.set("v", "<c-_>", "gc", { remap = true })

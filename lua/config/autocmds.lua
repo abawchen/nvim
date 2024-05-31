77b098c2b2f05ea6e0ce251aaa676319852a8971
abaw.chen <abaw.chen@gmail.com>
2024-05-31 09:31:26 +0800

    feat: auto-organize-imports on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_create_autocmd("BufWritePre", {
+  -- https://github.com/neovim/neovim/issues/24168#issue-1775351824
+  -- pattern = "*.java",
+  -- command = "lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })",
+  callback = function()
+    if vim.bo.ft == "java" then
+      vim.lsp.buf.code_action({
+        context = { only = { "source.organizeImports" } },
+        apply = true,
+      })
+    end
+  end,
+})
